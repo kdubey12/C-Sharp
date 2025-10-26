7d99d4635c432f758bb4136117fea90d1d003037
Kshitij Dubey <dubeyk@stpaulsschool.org.uk>
2025-10-26 01:27:40 +0000

    Test Commit

--- Strings/obj/Debug/net9.0/Strings.AssemblyInfo.cs
+++ Strings/obj/Debug/net9.0/Strings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f385bc99bd5cf9912d30bcffc76f391b15aa5cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TextFiles/obj/Debug/net9.0/TextFiles.AssemblyInfo.cs
+++ TextFiles/obj/Debug/net9.0/TextFiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f385bc99bd5cf9912d30bcffc76f391b15aa5cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
